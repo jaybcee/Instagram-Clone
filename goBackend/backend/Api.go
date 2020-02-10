c741b38a6c778fe4fdc5e5d33ca47c8a9d250d9f
Alexander Shevchenko <alexander_shev@mail.ru>
2020-02-09 20:33:43 -0500

    remove import from Api.go

--- goBackend/backend/Api.go
+++ goBackend/backend/Api.go
@@ -1,14 +1,8 @@
 package main
 
-import (
-	"log"
-
-	"github.com/fvbock/endless"
-)
-
 func api() {
 	r := createRoutes()
-	r.run(":3030")
+	r.Run(":3030")
 	//if err := endless.ListenAndServe("localhost:3030", r); err != nil {
 	//	log.Fatal(err)
 	//}
