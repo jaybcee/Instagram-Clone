62f457b627dd5c84149f8c00d982e005afe4e407
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-02-12 20:52:15 -0500

    revert some ui changes

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,51 +1,67 @@
 <template>
   <v-content>
-    <v-card class="elevation-12">
-      <v-toolbar
-        color="primary"
-        dark
-        flat
+    <v-container
+      class="fill-height"
+      fluid
+    >
+      <v-row
+        align="center"
+        justify="center"
       >
-        <v-toolbar-title>Login form</v-toolbar-title>
-        <v-spacer />
-        <v-tooltip bottom>
-          <span>Source</span>
-        </v-tooltip>
-        <v-tooltip right>
-          <span>Codepen</span>
-        </v-tooltip>
-      </v-toolbar>
-      <v-card-text>
-        <v-form @keyup.enter.native="login">
-          <v-text-field
-            id="email"
-            v-model="loginInfo.email"
-            label="Login"
-            name="login"
-            prepend-icon="mdi-email"
-            type="text"
-          />
-
-          <v-text-field
-            id="password"
-            v-model="loginInfo.password"
-            label="Password"
-            name="password"
-            prepend-icon="mdi-lock"
-            type="password"
-          />
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer />
-        <v-btn
-          color="primary"
-          @click="login"
+        <v-col
+          cols="12"
+          sm="8"
+          md="4"
         >
-          Login
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+          <v-card class="elevation-12">
+            <v-toolbar
+              color="primary"
+              dark
+              flat
+            >
+              <v-toolbar-title>Login form</v-toolbar-title>
+              <v-spacer />
+              <v-tooltip bottom>
+                <span>Source</span>
+              </v-tooltip>
+              <v-tooltip right>
+                <span>Codepen</span>
+              </v-tooltip>
+            </v-toolbar>
+            <v-card-text>
+              <v-form @keyup.enter.native="login">
+                <v-text-field
+                  id="email"
+                  v-model="loginInfo.email"
+                  label="Login"
+                  name="login"
+                  prepend-icon="mdi-email"
+                  type="text"
+                />
+
+                <v-text-field
+                  id="password"
+                  v-model="loginInfo.password"
+                  label="Password"
+                  name="password"
+                  prepend-icon="mdi-lock"
+                  type="password"
+                />
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                color="primary"
+                @click="login"
+              >
+                Login
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-content>
 </template>
 
