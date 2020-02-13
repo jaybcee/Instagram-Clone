e072a23c032e0e8220e3913a7f7a635a234166c5
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-02-13 13:12:01 -0500

    change signup resp

--- goBackend/backend/Routes.go
+++ goBackend/backend/Routes.go
@@ -19,9 +19,7 @@ func signupRoute(c *gin.Context) {
 
 	check(addingUserError)
 
-	c.JSON(200, struct {
-		Status int `json:"status"`
-	}{200})
+	c.String(200, "Success")
 }
 
 func testRoute(c *gin.Context) {
