e907c7484f6c86ba19f9071f3b2bba20e1629f4b
Laetitia Segaux <laetitia_segaux@hotmail.com>
2020-02-14 06:06:13 +0000

    Removed useless comment

--- frontend/src/views/PostPicture.vue
+++ frontend/src/views/PostPicture.vue
@@ -53,21 +53,6 @@
 
 <script>
 import axios from 'axios';
-// function getCookie(cname) {
-//   const name = `${cname}=`;
-//   const decodedCookie = decodeURIComponent(document.cookie);
-//   const ca = decodedCookie.split(';');
-//   for (let i = 0; i < ca.length; i += 1) {
-//     let c = ca[i];
-//     while (c.charAt(0) === ' ') {
-//       c = c.substring(1);
-//     }
-//     if (c.indexOf(name) === 0) {
-//       return c.substring(name.length, c.length);
-//     }
-//   }
-//   return '';
-// }
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   const expires = `expires=${d.toUTCString}`;
