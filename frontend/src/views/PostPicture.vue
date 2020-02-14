1d54421234026e0e9c32f4504dd9dbac1a0ae60a
Alexander Shevchenko <alexander_shev@mail.ru>
2020-02-13 19:07:02 -0500

    Add gray filter and Sobel filter to backend and frontend Merge conflict

--- frontend/src/views/PostPicture.vue
+++ frontend/src/views/PostPicture.vue
@@ -1,21 +1,53 @@
 <template>
   <div>
-    <div id="uploadPic"> <br><br>
-        <h2>Upload an image</h2> <br>
-        <div v-if="!image" class="postBox">
-            <div class="chooseFile">
-            <input type="file"  accept="image/x-png,image/jpeg"
-            ref="file" v-on:change="fileUpload()">
-            </div>
-        </div>
-        <div>
-            <input type="text" v-on:input="caption = $event.target.value"
-            placeholder="Enter your Caption">
-        </div>
-        <div class="submit">
-            <button v-on:click="submitFile()" class="submitButton">Submit</button>
-        </div>
-    </div>
+      <v-card
+    class="mx-auto"
+    max-width="500"
+    tile
+  >
+    <v-container fluid>
+      <v-row>
+        <v-col class="mainContainer">
+              <h2 class="uploadPictureHeader">Upload a picture</h2>
+              <div
+                class="base-image-input"
+                :style="{ 'background-image': `url(${imageData})` }"
+                @click="chooseImage"
+              >
+                <span
+                  v-if="!imageData"
+                  class="placeholder"
+                >
+                  Choose an Image
+                </span>
+                <input
+                  class="file-input"
+                  ref="fileInput"
+                  type="file"
+                  @input="onSelectFile"
+                >
+              </div>
+
+              <v-card-text class="text--primary">
+                    <div class="caption">
+                      <input type="text" v-on:input="caption = $event.target.value"
+                      placeholder="Enter your Caption">
+                    </div>
+            </v-card-text>
+
+            <v-row>
+                <v-checkbox class="checkbox" v-model="filterBnW" :label="`Filter Black and White`"></v-checkbox>
+                <v-checkbox v-model="filterSurprise" :label="`Filter Surpise me`"></v-checkbox>
+            </v-row>
+
+
+          <div class="submit">
+            <v-btn v-on:click="submitFile()" class="submitButton">Submit</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
   </div>
 </template>
 
@@ -49,6 +81,8 @@ export default {
   data() {
     return {
       file: '',
+      filterBnW: false,
+      filterSurprise: false,
       caption: '',
     };
   },
@@ -61,7 +95,10 @@ export default {
       const formData = new FormData();
       formData.append('file', this.file);
       formData.append('caption', this.caption);
-      setCookie('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluIiwiZXhwIjoxNjEzMDk3OTg3LCJvcmlnX2lhdCI6MTU4MTU2MTk4N30.79ko3o7zMggCUAPjAurWg-SdBdSHw8CY3r8DFgPoehk', 365);
+      formData.append('filterBnW', this.filterBnW);
+      formData.append('filterSurprise', this.filterSurprise);
+
+      setCookie('nothing', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluIiwiZXhwIjoxNjEzMDk3OTg3LCJvcmlnX2lhdCI6MTU4MTU2MTk4N30.79ko3o7zMggCUAPjAurWg-SdBdSHw8CY3r8DFgPoehk', 365);
 
       if (this.file !== '') {
         axios.post('http://localhost:3030/secure/api/uploadPhoto',
@@ -89,6 +126,22 @@ export default {
     text-align: center;
     align-content: center;
     margin-left: auto;
+}
+
+.checkbox{
+  margin-right: 20px;
+  margin-left: 40px;
+}
+
+.caption input{
+  border-bottom: solid 1px gray;
+  width: 100%;
+  padding-bottom: 4px;
+  text-align: center;
+}
+
+.caption input::placeholder{
+  text-align: center;
 }
 .submitButton{
     border: 1px solid;
