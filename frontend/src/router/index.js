26ac4fc35cf3914dcb9ddd6e9dd6a7941056f47a
Dina Khalesi <dinakhalesit@gmail.com>
2020-02-13 20:33:32 -0500

    adding router for user profile

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Signup from '../views/Signup.vue';
+import Profile from '../views/Profile.vue';
 
 Vue.use(VueRouter);
 
@@ -22,6 +23,11 @@ const routes = [
     name: 'Signup',
     component: Signup,
   },
+  {
+    path: '/user/:username',
+    name: 'Profile',
+    component: Profile,
+  }
 ];
 
 const router = new VueRouter({
